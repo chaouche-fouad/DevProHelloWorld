a2c7098e754a94b686b311b1f12e4f6878ef296e
chaouche-fouad <chaouchefouad9@gmail.com>
2024-07-20 22:02:18 +0100

    3 control and program flow if

---
+++ src/awesomeSauce/FlowIf.java
@@ -0,0 +1,16 @@
+package awesomeSauce;
+
+public class FlowIf {
+	public static void main(String[] args) {
+		int testNumber = -3;
+
+		if (testNumber <= 0) {
+			System.out.println("Less than or equal to zero");
+			
+		}
+		if(testNumber > 6) {
+			System.out.println("Greater than six");
+		}
+	}
+
+}
