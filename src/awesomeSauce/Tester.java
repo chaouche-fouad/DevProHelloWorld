461f2defa0b3c1aabf03f034ee65bcf220e0303d
chaouche-fouad <chaouchefouad9@gmail.com>
2024-07-20 21:20:33 +0100

    2 Language Basics

---
+++ src/awesomeSauce/Tester.java
@@ -0,0 +1,10 @@
+package awesomeSauce;
+
+public class Tester {
+	public static void main (String[] args) {
+		int circleRadius = 3;
+		
+		System.out.println("My circle has radius of " + circleRadius + " Units");
+	}
+
+}
